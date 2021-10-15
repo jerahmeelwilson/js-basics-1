3e7b3b380d1d97bf5593bbc13a54c0885f1d447f
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-10-15 17:08:41 -0500

    Added answers for array.js problems 1-4

--- js-basics-1/arrays.js
+++ js-basics-1/arrays.js
@@ -5,10 +5,16 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+const colorCopy = faveColors.slice();
+// console.log(colorCopy);
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push("yellow");
+// console.log(colorCopy);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,19 +25,50 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+const middleNums = numbers.splice(1,3);
+// console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
   Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
   which will be an array of numbers. 
-  Inside of the bigOrSmall function, create a new array called 'answers'. 
-  Then, loop over the passed in arr parameter, and check to see if the number in the
-  array is GREATER than 100. 
-  If it is, push 'big' as a string to the answers array. 
+
+  Inside of the bigOrSmall function, create a new array called 'answers'.
+
+  Then, loop over the passed in arr parameter,
+
+  and check to see if the number in the array is GREATER than 100. 
+
+  If it is, push 'big' as a string to the answers array.
+
   If the number is LESS than or EQUAL to 100, push 'small' as a string 
   to the answers array. 
+
   Return the answers array inside of the function.
 */
 
 // CODE HERE
+
+//function bigOrSmall with arr parameter
+function bigOrSmall(arr) {
+  //New array called answers
+  let answers = [];
+
+  //lop over passed in arr
+  for(let i = 0; i<arr.length;i++){
+    //check to see if the number in the array is GREATER than 100
+    if(arr[i] > 100){
+      //If it is, push 'big' as a string to the answers array.
+      answers.push("big");
+    } else {
+      // If the number is LESS than or EQUAL to 100, push 'small' as a string 
+      answers.push("small");
+    }
+  }
+  //Return the answers array inside of the function.
+  return answers;
+}
+
+//let numArray = [100,101,99,93,93,102,3];
+//console.log(bigOrSmall(numArray));
+//['small', 'big', 'small', 'small', 'small', 'big','small']
